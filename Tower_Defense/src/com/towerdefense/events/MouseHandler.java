2adcf695d07b700a0fb843aa4931d10b2268de18
balzrog <adrien.laffargue@viacesi.fr>
2015-06-04 20:05:35 +0200

    Modification du nom des objet de type MouseEvent

--- Tower_Defense/src/com/towerdefense/events/MouseHandler.java
+++ Tower_Defense/src/com/towerdefense/events/MouseHandler.java
@@ -6,31 +6,30 @@ import java.awt.event.MouseListener;
 public class MouseHandler implements MouseListener {
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
