0ff1f8d1cbd41ee8cb651b0b0965e1c12eae86f2
balzrog <adrien.laffargue@viacesi.fr>
2015-06-04 20:38:58 +0200

    Ajout de l'interface MouseMotionListener

--- Tower_Defense/src/com/towerdefense/events/MouseHandler.java
+++ Tower_Defense/src/com/towerdefense/events/MouseHandler.java
@@ -2,11 +2,18 @@ package com.towerdefense.events;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class MouseHandler implements MouseListener {
+public class MouseHandler implements MouseListener, MouseMotionListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
@@ -19,6 +26,12 @@ public class MouseHandler implements MouseListener {
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
