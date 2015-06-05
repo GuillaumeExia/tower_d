a9773ff757bbf9f59bbd7e95f8301504625fe667
Nicolas Dubois <nicolas.dubois1@viacesi.fr>
2015-06-05 15:51:11 +0200

    ajout de la map ajout de ground ajout du package DB

--- Tower_Defense/src/com/towerdefense/towerdefense/Map.java
+++ Tower_Defense/src/com/towerdefense/towerdefense/Map.java
@@ -1,5 +1,20 @@
 package com.towerdefense.towerdefense;
 
-public class Map {
+import java.util.ArrayList;
 
+public class Map {
+	private int width;
+	private int height;
+	private Ground[][] terrain;
+	private ArrayList<Tower> towers;
+	private ArrayList<Mob> mobs;
+
+	public Map() {
+
+	}
+
+	private void initialization(int width, int height, int id) {
+		this.width = width;
+		this.height = height;
+	}
 }
---
+++ Tower_Defense/src/com/towerdefense/towerdefense/database/DataBase.java
@@ -0,0 +1,24 @@
+package com.towerdefense.towerdefense.database;
+
+import com.towerdefense.towerdefense.Map;
+
+public class DataBase {
+	private DBLink database;
+
+	public DataBase() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Map loadMap(int id) {
+		return null;
+	}
+
+	public Map loadSave(int id) {
+		return null;
+	}
+
+	public void save(Map map) {
+
+	}
+}
