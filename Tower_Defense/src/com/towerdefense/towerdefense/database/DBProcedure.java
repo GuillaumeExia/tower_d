ae3f6e3c5127201e11c4dc768d9575f7cd7baa24
Nicolas Dubois <nicolas.dubois1@viacesi.fr>
2015-06-06 10:55:49 +0200

    DBProcedure get and set for scores, terrain, saves

--- Tower_Defense/src/com/towerdefense/towerdefense/database/DBProcedure.java
+++ Tower_Defense/src/com/towerdefense/towerdefense/database/DBProcedure.java
@@ -1,5 +1,28 @@
 package com.towerdefense.towerdefense.database;
 
-public class DBProcedure {
+public abstract class DBProcedure {
 
+	public static String getSave() {
+		return "{? = call getsave[(?, ?)]}";
+	}
+
+	public static String getScore() {
+		return "{? = call getscore[(?, ?)]}";
+	}
+
+	public static String getTerrain() {
+		return "{? = call getterrain[(?, ?)]}";
+	}
+
+	public static String setSave() {
+		return "{? = call setsave[(?, ?)]}";
+	}
+
+	public static String setScore() {
+		return "{? = call setscore[(?, ?)]}";
+	}
+
+	public static String setTerrain() {
+		return "{? = call setterrain[(?, ?)]}";
+	}
 }
