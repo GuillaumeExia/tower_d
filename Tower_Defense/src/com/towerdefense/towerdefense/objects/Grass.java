69989a0f40366baf6aa58d758f967866d3972e66
Nicolas Dubois <nicolas.dubois1@viacesi.fr>
2015-06-06 14:22:28 +0200

    new images folders and image uploading test

--- Tower_Defense/src/com/towerdefense/towerdefense/objects/Grass.java
+++ Tower_Defense/src/com/towerdefense/towerdefense/objects/Grass.java
@@ -1,5 +1,20 @@
 package com.towerdefense.towerdefense.objects;
 
-public class Grass {
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Grass extends Ground {
+
+	public Grass() {
+		super();
+		try {
+			setImage(ImageIO.read(new File("images/terrain/herbe.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
--- Tower_Defense/src/com/towerdefense/towerdefense/objects/Ground.java
+++ Tower_Defense/src/com/towerdefense/towerdefense/objects/Ground.java
@@ -12,4 +12,44 @@ public abstract class Ground {
 	public Ground() {
 		super();
 	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public boolean isConstructible() {
+		return constructible;
+	}
+
+	public boolean isSpawnable() {
+		return spawnable;
+	}
+
+	public boolean isWalkable() {
+		return walkable;
+	}
+
+	public void setConstructible(boolean constructible) {
+		this.constructible = constructible;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public void setSpawnable(boolean spawnable) {
+		this.spawnable = spawnable;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public void setWalkable(boolean walkable) {
+		this.walkable = walkable;
+	}
 }
